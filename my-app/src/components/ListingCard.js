be58f471089158385c514fbca0bd74ef1d1889fa
carlsachs <sachscarl@gmail.com>
2020-03-02 16:52:24 -0500

    added two components: listingpage and listingcard

---
+++ my-app/src/components/ListingCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListingCard () {
+    return (
+        <div>
+            <h1>This is an individual listing card</h1>
+        </div>
+    )
+}
---
+++ my-app/src/components/ListingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListingPage() {
+    return (
+        <div>
+            <h1>Header on the Listing Page</h1>
+        </div>
+    )
+}
