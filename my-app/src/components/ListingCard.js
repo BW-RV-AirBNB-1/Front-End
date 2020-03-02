4b7e2f9514f81f407655c12ffad4a7e6a243a8e4
carlsachs <sachscarl@gmail.com>
2020-03-02 17:02:13 -0500

    updated listing  card

--- my-app/src/components/ListingCard.js
+++ my-app/src/components/ListingCard.js
@@ -4,6 +4,11 @@ export default function ListingCard () {
     return (
         <div>
             <h1>This is an individual listing card</h1>
+            <h2>Location:</h2>
+            <h2>Description:</h2>
+            <h3>Price:</h3>
+            <h2>Reserved:</h2>
+            <h4>Insert Photo</h4>
         </div>
     )
 }
