3b6b0e3b386fcc52e9c74d87633c86ef0fbe4f8d
carlsachs <sachscarl@gmail.com>
2020-03-02 20:49:07 -0500

    added a Home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Home from "./components/Home.js";
+
 function App() {
   return (
     <div className="App">
